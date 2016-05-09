2669dc0afa485c38937138b717407f539e6177e6
hainam217 <hnngu23@student.monash.edu>
2016-05-09 10:44:46 +1000

    Update addlocationPage.js
    Add documentation

--- addlocationPage.js
+++ addlocationPage.js
@@ -1,10 +1,14 @@
 // Code for the Add Location page.
 
+//link the function to the save button when it is clicked
 document.getElementById('SaveButton').onclick = Save();
 
 function Save(){
+    //make variable that connect to the input by user
     var location = document.getElementById("txtLocation").innerHTML;
     var Nickname = document.getElementById("txtNickname").innerHTML;
+    
+    //save the input by user the local storage
     localStorage.setItem('location',location);
     localStorage.setItem('nickname',Nickname);
 }
