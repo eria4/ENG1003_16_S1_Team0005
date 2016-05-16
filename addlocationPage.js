200d44b6d3ba9b5a468dc4c4a9564c8f8ce49e2e
hainam217 <hnngu23@student.monash.edu>
2016-05-16 22:04:49 +1000

    Update addlocationPage.js

--- addlocationPage.js
+++ addlocationPage.js
@@ -1,100 +1,63 @@
-<!doctype html>
-<html>
-    <head>
-        <title>Weather</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
-        <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.1.3/material.indigo-pink.min.css">
-        <script src="https://storage.googleapis.com/code.getmdl.io/1.1.3/material.min.js"></script>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-        <link rel="stylesheet" href="css/weatherapp.css" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <link rel="shortcut icon" sizes="196x196" href="images/icon.png" />
-        <link rel="apple-touch-icon-precomposed" href="images/icon.png" />
-        <style>
-      html, body {
-        height: 50%;
-        margin: 0;
-        padding: 0;
-      }
-      #map {
-        height: 50%;
-      }
-      #floating-panel {
-        position: absolute;
-        top: 10px;
-        left: 25%;
-        z-index: 5;
-        background-color: #fff; 
-        padding: 5px;
-        border: 1px solid #999;
-        text-align: center;
-        font-family: 'Roboto','sans-serif';
-        line-height: 30px;
-        padding-left: 10px;
-      }
-    </style>
-    </head>
+// Code for the Add Location page.
+
+
+
+//link the function to the save button when it is clicked
+document.getElementById("SaveButton").onclick = Save();
+
+location = "Melbourne";
+location = "s";
+
+function Save(){
+    //make variable that connect to the input by user
+    this.location = document.getElementById("inputLocation").value;
+    var Nickname = document.getElementById("inputNickname").value;
+    var locationID = 1;
+    var nicknameID = 1;
+    location = JSON.stringify(location);
+    Nickname = JSON.stringify(Nickname);
     
-    <body>
-         <!--   Map   --> 
-        <div id='map'></div>
-        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-          <header class="mdl-layout__header">
-              <button onclick="location.href = 'index.html';" class="mdl-layout-icon mdl-button mdl-js-button mdl-button--icon"> 
-                <i class="material-icons">arrow_back</i>
-              </button>
-            <div class="mdl-layout__header-row">
-              <span class="mdl-layout-title" id="headerBarTitle">Add Location</span>
-            </div>
-          </header>
-          <main class="mdl-layout__content">
-            <div class="content-grid mdl-grid">
-              <div class="mdl-cell">
-                <!--   Main page content here:   -->
-                  <!-- Location -->
-                  <span class="mdl-layout-title"id="headerBarTitle">Type a Location</span>
-                  <!-- Textfield: Location -->
-                   
-                    <form action="#">
-                      <div class="mdl-textfield mdl-js-textfield">
-                        <input class="mdl-textfield__input" type="text" id="location">
-                        <label class="mdl-textfield__label" for="sample1">Insert Location</label>
-                      </div>
-                    </form>
-                  
-                  <!-- Textfield: Nickname -->
-                  <span class="mdl-layout-title"id="headerBarTitle">Nickname</span>
-                  
-                  <!-- Textfield: Nickname -->
-                   <form action="#">
-                      <div class="mdl-textfield mdl-js-textfield">
-                        <input class="mdl-textfield__input" type="text" id="nickname">
-                        <label class="mdl-textfield__label" for="sample1">Insert Nickname</label>
-                      </div>
-                    </form>
-                  
-                  
-                  
-                  <!-- "Add Location" button -->
-                <button class="mdl-button mdl-js-button mdl-button--raised"id="saveButton">
-                  Button
-                </button>
-                  
-                  <button onclick="findit()" id="submit"> find it! </button> 
+    
+    
+    //save the input by user the local storage
+    localStorage.setItem('location' + locationID,location);
+    localStorage.setItem('nickname' + nicknameID,Nickname);
+    
+    locationID++;
+    nicknameID++;
+    
+    
+}
 
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: {lat: -34.397, lng: 150.644}
+        });
+       
+    
+    var geocoder= new google.maps.Geocoder();
+            
+        document.getElementById('submit').addEventListener("click", function() {
+            findit(geocoder,map)
+        });
+    
+      }
 
-              </div>
-            </div>
-          </main>    
-        </div>
-        
-        <!--   JavaScript files:   -->
-         <script async defer
-  src="https://maps.googleapis.com/maps/api/js?v=3&amp;key=&amp;callback=initMap">
-    </script>
-        <script src="js/locationWeatherCache.js"></script>
-        <script src="js/addlocationPage.js"></script>
-    </body>
-</html>
+ function findit(geocoder, resultsMap) {
+                var address= document.getElementById("location").value;
+                geocoder.geocode({'address':address}, function(results, status) {
+                    if (status===google.maps.GeocoderStatus.OK){
+                        
+                resultsMap.setCenter(results[0].geometry.location);
+                resultsMap.panTo(results[0].geometry.location);      
+               var marker = new google.maps.Marker({
+               map: resultsMap,
+               position: results[0].geometry.location
+             });
+            } 
+                    else {
+      alert('Geocode was not successful for the following reason: ' + status);
+           }
+    });
+ }
