5117a278599e1cabcb58655c2c24c3e0b354940b
hainam217 <hnngu23@student.monash.edu>
2016-05-16 23:12:07 +1000

    Update addlocationPage.js

--- addlocationPage.js
+++ addlocationPage.js
@@ -55,6 +55,8 @@ function initMap() {
                map: resultsMap,
                position: results[0].geometry.location
              });
+                var lat = results[0].geometry.location.lat();
+                var lng = results[0].geometry.location.lng();
             } 
                     else {
       alert('Geocode was not successful for the following reason: ' + status);
