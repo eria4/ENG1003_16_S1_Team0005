ec46a80276f3ba21799034897fb97c2bd23c39b6
hainam217 <hnngu23@student.monash.edu>
2016-05-17 16:07:15 +1000

    Update addlocationPage.js

--- code/js/addlocationPage.js
+++ code/js/addlocationPage.js
@@ -1,43 +1,43 @@
 // Code for the Add Location page.
 
-
-
-//link the function to the save button when it is clicked
+//Link the function to the add location button when it is clicked
 document.getElementById("saveButton").onclick = function (){Save()};
 
 
+//Function to save location, nickname, latitude and longitude of location to local storage
 function Save(){
     //make variable that connect to the input by user
     var location = document.getElementById("location").value;
     var nickname = document.getElementById("nickname").value;
-    var locationID = 1;
-    var nicknameID = 1;
-    location = JSON.stringify(location);
-    nickname = JSON.stringify(nickname);
+    
+    //Object that contain the location, nickname, latitude and longitude of location
+    var saveobject = {
+        'location': location,
+        'nickname': nickname,
+        'lat': lat,
+        'lng': lng     
+    };
     
     
+    //Save the input by user the local storage
+    localStorage.setItem('saveobject',JSON.stringify(saveobject));
     
-    //save the input by user the local storage
-    localStorage.setItem('location' + locationID,location);
-    localStorage.setItem('nickname' + nicknameID,nickname);
-    
-    locationID++;
-    nicknameID++;
-    alert('saved')
+    //The main page will appear when add location is clcked
+    history.back();
     
 }
 
-//Function that initisl
+//Function that initialise map
 function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-
-          center: {lat: -34.397, lng: 150.644}
+            zoom: 12,
+            center: {lat: -34.397, lng: 150.644}
         });
        
     
     var geocoder= new google.maps.Geocoder();
-            
+        
+    //Code that connect the input in location text box with the function findit
         document.getElementById('location').addEventListener("input", function() {
             findit(geocoder,map)
         });
@@ -45,7 +45,8 @@ function initMap() {
       }
 
 
-
+//Function that change the location that appears on the map 
+//when text is being typed in the location text box
 function findit(geocoder, resultsMap) {
                 var address = document.getElementById("location").value;
                 geocoder.geocode({'address':address}, function(results, status) {
@@ -57,8 +58,9 @@ function findit(geocoder, resultsMap) {
                map: resultsMap,
                position: results[0].geometry.location
              });
-                var lat = results[0].geometry.location.lat();
-                var lng = results[0].geometry.location.lng();
+                //Global variable that contain the latitude and longitude of the location
+                this.lat = results[0].geometry.location.lat();
+                this.lng = results[0].geometry.location.lng();
             } 
                     
     });
