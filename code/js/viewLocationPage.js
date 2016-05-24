ad429ea6ed25102cf4d3fdef3d37d7542ab34b5e
hainam217 <hnngu23@student.monash.edu>
2016-05-24 11:54:40 +1000

    Update viewLocationPage.js

--- code/js/viewLocationPage.js
+++ code/js/viewLocationPage.js
@@ -10,3 +10,18 @@ if (locationIndex !== null)
     // If a location name was specified, use it for header bar title.
     document.getElementById("headerBarTitle").textContent = locationNames[locationIndex];
 }
+
+loadLocations();
+var lat = locations[index].latitude;
+var lng = locations[index].longitude;
+
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: {lat: -34.397, lng: 150.644}
+        });
+       
+    
+ 
+    
+      }
