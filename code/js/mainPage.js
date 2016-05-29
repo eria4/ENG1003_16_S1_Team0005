cc03dac96797c3c9958f93669844a70c66a254a6
hainam217 <hnngu23@student.monash.edu>
2016-05-29 13:36:09 +1000

    Update mainPage.js

--- code/js/mainPage.js
+++ code/js/mainPage.js
@@ -3,25 +3,25 @@
 // This is sample code to demonstrate navigation.
 // You need not use it for final app.
 
-function viewLocation(locationName)
+function viewLocation(locationIndex)
 {
-    // Save the desired location to local storage
-    localStorage.setItem(APP_PREFIX + "-selectedLocation", locationName); 
     // And load the view location page.
     location.href = 'viewlocation.html';
 }
 
 
 window.onload = function getItem(){
-    var saveobject = localStorage.getItem('saveobject');
-    
+    //Get saved datas from local storage
+    var saveobject = localStorage.getItem('weatherApp');
+    //Parse the data
     saveobject = JSON.parse(saveobject);
-    
-    if (saveobject.nickname != ""){
-        document.getElementById("location1").textContent = saveobject.nickname.toUpperCase();
-    }
-    else {
-        document.getElementById("location1").textContent = saveobject.location.toUpperCase();
-    }
+
+    //Make the nickname in the data appear on the lunch page
+    document.getElementById("location1").textContent = saveobject[1].nickname.toUpperCase();
+    document.getElementById("location2").textContent = saveobject[2].nickname.toUpperCase();
+    document.getElementById("location3").textContent = saveobject[3].nickname.toUpperCase();
+    document.getElementById("location4").textContent = saveobject[4].nickname.toUpperCase();
+    document.getElementById("location5").textContent = saveobject[5].nickname.toUpperCase();
+    document.getElementById("location6").textContent = saveobject[6].nickname.toUpperCase();
     
 }
